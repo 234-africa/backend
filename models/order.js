561ee8d2c6cfcf6087a5e33d25aa22c294a01fcd
234-africa <234groupinfo@gmail.com>
2025-11-08 01:07:18 +0100

    Update order.js

--- models/order.js
+++ models/order.js
@@ -14,6 +14,7 @@ const OrderSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
   title: {
     type: String,
     required: true
